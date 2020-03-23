c7be81cb770c1eab8f4c293f82ad1d66c156333e
hmcdonald2 <harrymcdonald@gmail.com>
2020-03-23 22:39:07 +1000

    a

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, date
+from time import localtime, time, asctime
 
 app = Flask(__name__)
 app.static_folder = 'static'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+testlist = [1, 2, 4]
+testvar = 'testme'
 
 # class Todo(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
@@ -16,9 +19,36 @@ db = SQLAlchemy(app)
 #     def __repr__(self):
 #         return '<Task %r>' % self.id
 
-@app.route('/', methods=['POST', 'GET'])
+namelist =  ['Johnson', 'Jackson', 'Smith', 'Frank', 'Wilson']
+attendtype = ['Call In', 'Call Out', 'Conference', 'Notes', 'Research']
+attendon = ['Client', 'Other Side', 'Counsel', 'Other']
+today = date.today()
+largetime = asctime(localtime(time()))
+smalltime = largetime[11:13] + "." + largetime[14:16]
 
+def sortnamelist():
+    namelist.sort()
+    return namelist
+
+sortnamelist()
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    return render_template('index.html', namelist=namelist, attendtype=attendtype, attendon=attendon, today=today,
+                           smalltime=smalltime)
+
+@app.route('/previewfn', methods=['GET', 'POST'])
+def previewfn():
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Submit':
+            return render_template('previewfn')
+        else:
+            pass
+    elif request.method == 'GET':
+        return render_template('previewfn')
+
+
+    # def index():
     # if request.method == 'POST':
     #     task_content = request.form['content']
     #     new_task = Todo(content=task_content)
@@ -32,7 +62,7 @@ def index():
     #
     # else:
     #     tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html')
+    #     return render_template('index.html', namelist = namelist, attendtype = attendtype, attendon = attendon, today = today, smalltime = smalltime)
     #, tasks = tasks)
 
 if __name__ == "__main__":
